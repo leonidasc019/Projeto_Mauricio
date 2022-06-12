9b0ad9ff6717ceb9aaefaa832201199ed8a274a4
Jeander2001 <jeander.santos@aluno.unifenas.br>
2022-06-11 22:33:12 -0300

    tela de login completa

--- projeto/lib/main.dart
+++ projeto/lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:projeto/widgets/login.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const PaginaLogin(),
+      //  initialRoute: '/home',
+      routes: {
+        '/home': (context) => const PaginaLogin(),
+      },
+    );
+  }
+}
