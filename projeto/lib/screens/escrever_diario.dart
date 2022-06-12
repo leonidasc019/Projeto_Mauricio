bc49b2e97dc85c1d72aded42c67e52012ba7a05f
edmar2020 <72048789+edmar2020@users.noreply.github.com>
2022-06-12 00:31:55 -0300

    Alteracao_2

--- projeto/lib/screens/escrever_diario.dart
+++ projeto/lib/screens/escrever_diario.dart
@@ -35,7 +35,7 @@ class _PaginaEscreverState extends State<PaginaEscrever> {
                   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
                 ),
                 SizedBox(
-                  width: 155,
+                  width: 150,
                   child: Column(
                     children: [
                       logo,
